7ecf0bf483732dcda5b1142d489b813f6d272232
Michael Pu <michael.pu123@gmail.com>
2019-01-11 11:12:00 -0500

    Add loading

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,12 +4,34 @@ import axios from 'axios';
 
 export default class HomePage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            doorbell: false,
+            tray: false
+        };
+    }
+
     async handleDoorbell() {
-        await axios.post('/api/doorbell');
+        this.setState({
+            doorbell: true
+        }, async () => {
+            await axios.post('/api/doorbell');
+            this.setState({
+                doorbell: false
+            });
+        });
     }
 
     async handleCDTray() {
-        await axios.post('/api/cdtray');
+        this.setState({
+            tray: true
+        }, async () => {
+            await axios.post('/api/cdtray');
+            this.setState({
+                tray: false
+            });
+        });
     }
 
     render() {
@@ -17,8 +39,28 @@ export default class HomePage extends Component {
             <div className="container mt-3">
                 {localStorage.token ?
                     <div>
-                        <button className="btn btn-primary btn-lg btn-block" onClick={this.handleDoorbell}>Doorbell</button>
-                        <button className="btn btn-primary btn-lg btn-block" onClick={this.handleCDTray}>CD Tray</button>
+                        <button className="btn btn-primary btn-lg btn-block" onClick={this.handleDoorbell}>
+                            {this.state.doorbell ?
+                                <div>
+                                    <i className='fa fa-circle-o-notch fa-spin mr-2'></i>
+                                    Doorbell
+                                </div> :
+                                <div>
+                                    Doorbell
+                                </div>
+                            }
+                        </button>
+                        <button className="btn btn-primary btn-lg btn-block" onClick={this.handleCDTray}>
+                            {this.state.tray ?
+                                <div>
+                                    <i className='fa fa-circle-o-notch fa-spin mr-2'></i>
+                                    CD Tray
+                                </div> :
+                                <div>
+                                    CD Tray
+                                </div>
+                            }
+                        </button>
                     </div> :
                     <LoginPage />
                 }
