9dbcb0883bb0b849e88b49aad1f21551c901cbf4
Michael Pu <michael.pu123@gmail.com>
2019-01-15 16:50:03 -0500

    Add get CD tray status

---
+++ backend/actions/traystatus.c
@@ -0,0 +1,22 @@
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <linux/cdrom.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc,char **argv) {
+	int cdrom;
+
+	if ((cdrom = open("/dev/sr0",O_RDONLY | O_NONBLOCK)) < 0) {
+		fprintf(stderr, "Unable to open device %s",argv[1]);
+		return 1;
+	}
+
+	if (ioctl(cdrom,CDROM_DRIVE_STATUS) == CDS_TRAY_OPEN) {
+		printf("open\n");
+	} else {
+		printf("close\n");
+	}
+
+	close(cdrom);
+}
