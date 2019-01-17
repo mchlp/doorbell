f648d9bfb4dcc5b0ac995bc81159d24c1c981721
Michael Pu <michael.pu123@gmail.com>
2019-01-16 19:58:58 -0500

    Update build script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd frontend
 npm run build
 rsync ./build/* alpha:/srv/doorbell/frontend -avz --delete-after
 cd ../backend
-rsync . alpha:/srv/doorbell/backend -avz --exclude 'node/modules'
+rsync . alpha:/srv/doorbell/backend -avz --exclude 'node_modules'
 ssh alpha "cd /srv/doorbell/backend; npm install; cd actions; gcc traystatus.c -o traystatus; sudo systemctl restart doorbell"
