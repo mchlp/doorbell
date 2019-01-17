fb5b4a91eda548a1291744ece00e9407ddfcb713
Michael Pu <michael.pu123@gmail.com>
2019-01-16 19:55:08 -0500

    Added voice to doorbell

--- backend/app.mjs
+++ backend/app.mjs
@@ -71,7 +71,7 @@ async function startCheckOccupied(res) {
                 stopCheckOccupied(true);
                 return;
             }
-            exec('espeak "Close the CD Drive."');
+            exec('espeak "If you hear this, close the CD Drive." -s 160');
             state.occupiedCheck.checkInterval = setInterval(checkOccupied, 500);
         });
     }
@@ -124,6 +124,7 @@ app.post('/api/login', async function (req, res, next) {
 
 app.post('/api/doorbell', isAuthenticated, async function (req, res, next) {
     await exec('./actions/ring.sh');
+    await exec('espeak "Someone is at the door." -s 160');
     res.json({
         'status': 'success'
     });
